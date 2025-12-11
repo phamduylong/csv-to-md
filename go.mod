1971813e537c366bd94c0224bbb7d79ee22af87b
longph <longphamduy2002@gmail.com>
2025-12-11 23:50:40 +0200

    Base implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module csv-to-md
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	res, err := convert(
+		`First name,Last name,Email,Phone
+Jane,Smith,jane.smith@email.com,555-555-1212
+John,Doe,john.doe@email.com,555-555-3434
+
+Alice,Wonder,alice@wonderland.com,555-555-5656
+Aaron, Potter`)
+	if err != nil {
+		fmt.Printf("An error occurred 🙄: %s", err.Error())
+	} else {
+		fmt.Printf("Converted successfully ✅ Result:\n%s", res)
+	}
+
+}
+
+// convert csv string into a markdown table
+func convert(csv string) (string, error) {
+	if csv == "" {
+		return "", errors.New("empty CSV input")
+	}
+	result := ""
+	maxCol := 0
+	nrOfCols := 0
+	const emptyCol = "| "
+	for line := range strings.Lines(csv) {
+		if line == "" {
+			continue
+		}
+		nrOfCols = strings.Count(line, ",") + 1
+		if nrOfCols > maxCol {
+			maxCol = nrOfCols
+		}
+	}
+
+	lines := strings.Split(csv, "\n")
+
+	for idx := range len(lines) {
+		originalLine := strings.ReplaceAll(lines[idx], "\n", "")
+		colVals := strings.Split(originalLine, ",")
+		newLine := ""
+
+		if originalLine == "" {
+			newLine = ""
+		}
+
+		for i := 0; i < maxCol; i++ {
+			if i < len(colVals) {
+				newLine += fmt.Sprintf("| %s ", colVals[i])
+			} else {
+				newLine += emptyCol
+			}
+		}
+
+		// add final column closer and new line
+		newLine += "|\n"
+		// replace awkward double spaces
+		newLine = strings.ReplaceAll(newLine, "  ", " ")
+		// append to result string
+		result += newLine
+
+		if idx == 0 {
+			separatorLine := constructSeparatorLine(maxCol)
+			result += separatorLine
+		}
+	}
+
+	return result, nil
+}
+
+// Construct a separator line between the header line and data lines
+func constructSeparatorLine(colsCount int) string {
+	separatorLine := "| "
+	for range colsCount {
+		separatorLine += "- | "
+	}
+	separatorLine += "\n"
+	return separatorLine
+}
