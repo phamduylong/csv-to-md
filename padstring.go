2745b9d526c1f31ec71ea4876f800d7174c05720
longph <longphamduy2002@gmail.com>
2025-12-16 21:43:01 +0200

    fix padcenter adding more to the end

--- padstring.go
+++ padstring.go
@@ -47,7 +47,6 @@ func padEnd(originalString string, desiredLen int, paddingChar rune) (string, er
 }
 
 // Pad both sides. If odd characters are to be padded, the longer string is padded to the start of the string.
-// Usually the longer string is actually padded to the end, but this serves the purpose of this utility class
 func padCenter(originalString string, desiredLen int, paddingChar rune) (string, error) {
 	if len(originalString) > desiredLen {
 		return "", errors.New(padLengthErrorString)
@@ -55,8 +54,8 @@ func padCenter(originalString string, desiredLen int, paddingChar rune) (string,
 
 	lenDiff := desiredLen - len(originalString)
 
-	toPadEnd := lenDiff / 2
-	toPadStart := lenDiff - toPadEnd
+	toPadStart := lenDiff / 2
+	toPadEnd := lenDiff - toPadStart
 
 	resStr := originalString
 	resStr, err := padEnd(originalString, len(resStr)+toPadEnd, paddingChar)
